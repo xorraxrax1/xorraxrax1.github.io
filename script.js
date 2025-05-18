9ac9752d194f49cd62590dc8532fa92e68e490a4
0xffffff <76949603+xorraxrax1@users.noreply.github.com>
2025-05-18 23:53:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ const observer = new IntersectionObserver((entries) => {
             entry.target.classList.add('fade-in');
             observer.unobserve(entry.target);
         }
-// script monitoring test!
+// admin
     });
 }, observerOptions);
 
